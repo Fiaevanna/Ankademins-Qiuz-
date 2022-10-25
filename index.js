5eda7a105279f7a22041c73a50b45ba1c8c75df8
fiaevanna <fiaevanna@gmail.com>
2022-10-25 15:14:42 +0200

    Added one question & built logic for score counting & clicking submit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+/* här har jag skapat en funktion som innehåller min logik för att räkna poäng*/
+function countScore(){
+    let score = 0;
+    const questions = [
+        { elementName: "questionOneOption", correctAnswer: "true" }
+    ];
+
+   
+    questions.forEach((question) => {
+        const checkedElement = document.querySelector(
+          `input[name="${question.elementName}"]:checked`
+        );
+    
+        if (question.correctAnswer === checkedElement.value) {
+            score ++; 
+        }
+      });
+      console.log(score);
+}
+
+/* här har jag skapat en variabel som innehåller min submitknapp, 
+sen skapar jag en eventlyssnare på min knapp och den lyssnar efter klick och då körs countscore.
+ */
+
+    const submitButton = document.querySelector("#submit");
+    submitButton.addEventListener("click", countScore);
+
